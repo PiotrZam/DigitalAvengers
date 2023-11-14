07c6ae0d95e6c3a4e053906ac1458bd2a47ec9fa
Piotr Zambrzycki <Piotr.Zambrzycki@gegroup.com>
2023-11-14 01:14:07 +0000

    Saving and loading posts from the server, improved dashboard UI

--- server.js
+++ server.js
@@ -44,23 +44,63 @@ app.post('/login', (req, res) => {
     });
 });
 
-app.get('/posts', (req, res) => {
-    fs.readFile('sample_posts.json', 'utf8', (err, data) => {
+app.get('/getPosts', (req, res) => {
+    // Read posts from 'posts.json'
+    fs.readFile('posts.json', 'utf8', (err, data) => {
         if (err) {
-            console.log('Error...');
-            res.status(500).json({ error: 'Server error: Could not read post data...' });
-            return;
+            console.error('Error reading posts.json:', err.message);
+            return res.status(500).json({ error: 'Error fetching posts' });
         }
 
         try {
             const posts = JSON.parse(data);
-            res.status(200).json({ error: 'Invalid username or password' });
-
+            res.status(200).json(posts);
         } catch (error) {
-            res.status(500).json({ error: 'Server error: Could not parse login credentials' });
+            console.error('Error parsing posts.json:', error.message);
+            res.status(500).json({ error: 'Error fetching posts' });
         }
     });
- });
+});
+
+
+app.post('/addPost', (req, res) => {
+    const { title, content } = req.body;
+
+    if (!title || !content) {
+        return res.status(400).json({ success: false, error: 'Title and content are required' });
+    }
+
+    const newPost = {
+        author: 'Server Author', // You may modify this to get the actual author from the request
+        date: new Date().toLocaleDateString(),
+        title,
+        content,
+    };
+
+    // Read existing posts from 'posts.json'
+    let posts = [];
+    try {
+        const postsData = fs.readFileSync('posts.json', 'utf8');
+        posts = JSON.parse(postsData);
+    } catch (error) {
+        console.error('Error reading posts.json:', error.message);
+    }
+
+    // Add the new post
+    posts.unshift(newPost);
+
+    // Write the updated posts back to 'posts.json'
+    fs.writeFile('posts.json', JSON.stringify(posts, null, 2), (err) => {
+        if (err) {
+            console.error('Error writing to posts.json:', err.message);
+            return res.status(500).json({ success: false, error: 'Error saving post' });
+        }
+
+        res.status(200).json({ success: true });
+    });
+});
+
+
 
 // Start the server
 app.listen(port, () => {
